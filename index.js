950b901c99a47602ad0626e4bbc64e79ced68091
AuroPick <aykutsakibusiness@gmail.com>
2022-10-06 00:53:32 +0300

    react native plugin

--- index.js
+++ index.js
@@ -1,98 +1,98 @@
 module.exports = {
   parserOptions: {
-    ecmaVersion: "latest",
+    ecmaVersion: 'latest'
   },
-  plugins: ["react-native"],
-  extends: ["airbnb", "airbnb/hooks", "plugin:import/recommended", "prettier"],
+  plugins: ['@gokarma/react-native'],
+  extends: ['airbnb', 'airbnb/hooks', 'plugin:import/recommended', 'prettier'],
   rules: {
     // turn off prefer default export error
-    "import/prefer-default-export": "off",
+    'import/prefer-default-export': 'off',
 
     // import order
-    "import/order": [
-      "error",
+    'import/order': [
+      'error',
       {
-        groups: ["builtin", "external", "internal"],
+        groups: ['builtin', 'external', 'internal'],
         pathGroups: [
           {
-            pattern: "react",
-            group: "external",
-            position: "before",
+            pattern: 'react',
+            group: 'external',
+            position: 'before'
           },
           {
-            pattern: "*.png",
-            group: "sibling",
+            pattern: '*.png',
+            group: 'sibling',
             patternOptions: { matchBase: true },
-            position: "after",
-          },
+            position: 'after'
+          }
         ],
-        pathGroupsExcludedImportTypes: ["react"],
-        "newlines-between": "always",
+        pathGroupsExcludedImportTypes: ['react'],
+        'newlines-between': 'always',
         alphabetize: {
-          order: "asc",
-          caseInsensitive: true,
-        },
-      },
+          order: 'asc',
+          caseInsensitive: true
+        }
+      }
     ],
 
     // show error if default export
-    "import/no-default-export": "error",
+    'import/no-default-export': 'error',
 
     // ignore errors for prop types
-    "react/prop-types": "off",
+    'react/prop-types': 'off',
 
     // conflict with prettier
-    "react/jsx-curly-newline": "error",
+    'react/jsx-curly-newline': 'error',
 
     // use arrow function
-    "react/function-component-definition": [
-      "error",
+    'react/function-component-definition': [
+      'error',
       {
-        namedComponents: "arrow-function",
-        unnamedComponents: "arrow-function",
-      },
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function'
+      }
     ],
 
     // prevent eslint to complain about the "styles" variable being used before it was defined
-    "no-use-before-define": ["error", { variables: false }],
+    'no-use-before-define': ['error', { variables: false }],
 
     // show error if there is semicolon
-    semi: ["error", "never"],
+    semi: ['error', 'never'],
 
     // allow "_id" property
-    "no-underscore-dangle": ["error", { allow: ["_id"] }],
+    'no-underscore-dangle': ['error', { allow: ['_id'] }],
 
     // conflict with prettier
-    "object-curly-newline": "off",
+    'object-curly-newline': 'off',
 
     // do not use comma end of the object
-    "comma-dangle": ["error", "never"],
+    'comma-dangle': ['error', 'never'],
 
     // disable nested ternary error
-    "no-nested-ternary": "off",
+    'no-nested-ternary': 'off',
 
     // put newline
-    "newline-before-return": "error",
+    'newline-before-return': 'error',
 
     // put newline
-    "newline-after-var": ["error", "always"],
+    'newline-after-var': ['error', 'always'],
 
     // detect unused styles
-    "react-native/no-unused-styles": "error",
+    '@gokarma/react-native/no-unused-styles': 'error',
 
     // split platfrom components
-    "react-native/split-platform-components": "error",
+    '@gokarma/react-native/split-platform-components': 'error',
 
     // detect inline styles
-    "react-native/no-inline-styles": "error",
+    '@gokarma/react-native/no-inline-styles': 'error',
 
     // use colors as variable
-    "react-native/no-color-literals": "error",
+    '@gokarma/react-native/no-color-literals': 'error',
 
     // detect raw text
-    "react-native/no-raw-text": "error",
+    '@gokarma/react-native/no-raw-text': 'error',
 
     // detect unnecessary array style
-    "react-native/no-single-element-style-arrays": "error",
-  },
-};
+    '@gokarma/react-native/no-single-element-style-arrays': 'error'
+  }
+}
